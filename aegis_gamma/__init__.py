1e54d4b38adff7b4d06b1dd9d09cd18470b33517
Desgranges Yoann <desgranges06@gmail.com>
2026-04-27 14:40:55 +0200

    Update __init__.py

--- aegis_gamma/__init__.py
+++ aegis_gamma/__init__.py
@@ -1,1 +1,9 @@
 __version__ = "4.0.0"
+
+from aegis_gamma.core import *
+from aegis_gamma.modules import *
+from aegis_gamma.orchestrator import SystemeControleNarratifV4
+
+__all__ = [
+    "SystemeControleNarratifV4",
+]
